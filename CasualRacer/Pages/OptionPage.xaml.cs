858583a3fb269dcf911042415f10c13790ddeefe
tomwendel <tom.wendel@rabbitcode.de>
2015-09-28 14:27:03 +0200

    [001] Die ersten Pages, die erste Navigation, das Grundgerüst steht!

---
+++ CasualRacer/Pages/OptionPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CasualRacer.Pages
+{
+    /// <summary>
+    /// Interaction logic for OptionPage.xaml
+    /// </summary>
+    public partial class OptionPage : Page
+    {
+        public OptionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ CasualRacer/Pages/StartPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CasualRacer.Pages
+{
+    /// <summary>
+    /// Interaction logic for StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new GamePage());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new OptionPage());
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
+    }
+}
